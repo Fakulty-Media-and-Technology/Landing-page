0dfeab1d424fb8c386ed7e96e6b19c8ab9f635bd
Eboh Benjamin <156227731+Eboh4119@users.noreply.github.com>
2024-11-08 21:42:55 +0000

    benjamin 1st commit

---
+++ my-next-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="total-container bg-black h-[100vh]">
+      <div className="navbar">
+        <div className="logo text-white">
+          <h1>redplay</h1>
+        </div>
+      </div>
+    </div>
+  );
+}

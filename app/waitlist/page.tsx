8e8ad58e352623d934cb82ad815645fcccbb8467
Kingsley O. <102329150+kingvidoski@users.noreply.github.com>
2024-12-16 10:11:19 +0000

    fixed build error

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Wrapper from '@/components/Wrapper'
 import React from 'react'
 import Size from '@/utils/useResponsiveSize';
@@ -7,7 +9,9 @@ function page() {
   return (
     <main className='bg-black mt-2 pb-2 overflow-hidden'>
 
+
       <Wrapper className=''>
+
         <div style={{
           height: Size.getHeight() * 0.8
         }} className='mt-7 pt-3 overflow-hidden flex flex-cols items-center justify-center'>

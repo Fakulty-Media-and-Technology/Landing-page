5247974b4e43338ab7f59da39312a9ff93ec6e5a
Eddels <talk2bette@gmail.com>
2025-06-01 12:52:19 +0000

    Update page.tsx
    Added google Analytics tag

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ import HeroSection from "@/screens/Home/HeroSection";
 import LaunchSection from "@/screens/Home/LaunchSection";
 import React from "react";
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LLG8BVLBV9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LLG8BVLBV9');
+</script>
 
 export default function Home() {
   return (
